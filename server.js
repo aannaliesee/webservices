4b727c3561e906860f73e9be4e79250a35ca9b22
Annaliese Mits <annaliese.mits@gmail.com>
2024-01-13 10:44:33 +1100

    updated server.js

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/annaliese', (req, res) => {
     res.send('Annaliese Mits');
 });
+
 
 const port = 3000; 
 
