0a1401523acf962d119021bf36a6d24ca28339d7
Annaliese Mits <annaliese.mits@gmail.com>
2024-01-13 23:40:07 +1100

    test

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const name = "Annaliese Mits"; // Replace with the name you want to display
+    res.send(`Hello, ${name}!`);
 });
 
-app.get('/annaliese', (req, res) => {
-    res.send('Annaliese Mits');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-
-const port = 3000; 
-
-app.listen(process.env.port || port);
-console.log('Web server is listening at port ' + (process.env.port || port));
